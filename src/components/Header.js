f290c6f32e2da7303277eb78900802d59c69dcc0
hengameh <hengameh.bolouri@gmail.com>
2021-11-04 12:57:21 +0100

    commit1

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,8 +4,8 @@ import GitHubButton from 'react-github-btn'
 export default function Header() {
     return (
         <div style={{ textAlign:"center"}}>
-            <h1>Search Emoji <span></span></h1>
-            <p>Simple tool for search</p>
+            <h1>Search Emoji <span>&#x1F984;</span></h1>
+            <p>Simple tool for search emoji with ReactJS</p>
            <GitHubButton href= 'https://github.com/hbolouri' data-show-count={true} data-size='large' data-text='star' data-icon='octicon-star'>star</GitHubButton>
            <GitHubButton href='https://github.com/hbolouri' data-show-count={true} data-size='large' data-text='form' data-icon='octicon-repo-forked'>fork</GitHubButton>
         </div>
